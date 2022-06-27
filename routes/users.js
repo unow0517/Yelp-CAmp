8b0c8fce8d86cab35719e363d073ea4b9d76afeb
unow0517 <unow1992@gmail.com>
2022-06-27 20:26:43 +0200

    change to original code

--- routes/users.js
+++ routes/users.js
@@ -11,21 +11,7 @@ router.route('/register')
 router.route('/login')
   .get(user.logIn)
   .post(
-    passport.authenticate('local', (err, user, next) => {
-      if (err) {
-        return next(err); // will generate a 500 error
-      }
-      // Generate a JSON response reflecting authentication status
-      if (!user) {
-        return res.status(401).send({ error: 'Authentication failed' });
-      }
-      req.login(user, (err) => {
-        if (err) {
-          return next(err);
-        }
-        return res.status(202).send({ error: 'Authentication succeeded' });    
-      });
-    })
+    passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), user.postLogIn
   )
 
 router.get('/logout', user.getLogOut );
